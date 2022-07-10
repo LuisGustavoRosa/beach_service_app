d9326474d9fa10a963b3875a8eacb4b7d7b06dcb
Luis Gustavo Rosa Cardoso <44246590+LuisGustavoRosa@users.noreply.github.com>
2022-07-10 09:36:46 -0300

    Projeto Finalizado

--- config/database.js
+++ config/database.js
@@ -2,8 +2,6 @@
 
 const Env = use('Env')
 const Helpers = use('Helpers')
-const Url = require('url-parse')
-const DATABASE_URL = new Url(Env.get('DATABASE_URL'))
 
 module.exports = {
 
@@ -11,11 +9,11 @@ module.exports = {
   postgres: {
     client: 'postgres',
     connection: {
-      host: Env.get('DB_HOST', DATABASE_URL.hostname),
-      port: Env.get('DB_PORT', DATABASE_URL.port),
-      user: Env.get('DB_USER', DATABASE_URL.username),
-      password: Env.get('DB_PASSWORD',DATABASE_URL.password),
-      database: Env.get('DB_DATABASE', DATABASE_URL.pathname.substring(1))
+      host: Env.get('DB_HOST', 'localhost'),
+      port: Env.get('DB_PORT', '5432'),
+      user: Env.get('DB_USER', 'postgres'),
+      password: Env.get('DB_PASSWORD', 'postgres'),
+      database: Env.get('beach_service', 'beach_service')
     },
     debug: Env.get('DB_DEBUG', false)
   }
