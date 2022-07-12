d9aa1ba0f88fd79f52b4254c736d7ba6ac8012b0
Luis Gustavo Rosa Cardoso <44246590+LuisGustavoRosa@users.noreply.github.com>
2022-07-12 18:51:53 -0300

    make it better

--- config/database.js
+++ config/database.js
@@ -5,20 +5,16 @@ const Helpers = use('Helpers')
 
 module.exports = {
 
-  connection: Env.get('DB_CONNECTION', 'postgres'),
-  postgres: {
-    client: 'postgres',
+  connection: Env.get('DB_CONNECTION', 'pg'),
+  pg: {
+    client: 'pg',
     connection: {
-      host: Env.get('DB_HOST', ''),
+      host: Env.get('DB_HOST', 'localhost'),
       port: Env.get('DB_PORT', ''),
-      user: Env.get('DB_USER', ''),
+      user: Env.get('DB_USER', 'root'),
       password: Env.get('DB_PASSWORD', ''),
-      database: Env.get('DB_DATABASE', ''),
-      ssl: {
-        rejectUnauthorized: false
-      }
+      database: Env.get('DB_DATABASE', 'adonis')
     },
-
     debug: Env.get('DB_DEBUG', false)
   }
 }
