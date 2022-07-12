b2074d3fe39982fd2c1419fd50ae836cebc7f334
Luis Gustavo Rosa Cardoso <44246590+LuisGustavoRosa@users.noreply.github.com>
2022-07-12 17:07:02 -0300

    make it better

--- server.js
+++ server.js
@@ -14,11 +14,7 @@
 |
 | """ Preloading files """
 |     Also you can preload files by calling `preLoad('path/to/file')` method.
-|     Make sure to pass relative path from the project root.
-*/
-
-
-
+|    
 const { Ignitor } = require('@adonisjs/ignitor')
 new Ignitor(require('@adonisjs/fold'))
   .appRoot(__dirname)
